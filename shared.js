4fe96d7529183e327ae71355ec883668ced74bee
Daniel Son <dson.email@gmail.com>
2022-01-31 08:03:54 +1100

    Add modal closing functionality with JavaScript

--- shared.js
+++ shared.js
@@ -1,6 +1,7 @@
 const backdrop = document.querySelectorAll(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan button");
+const modalNoButton = document.querySelector(".modal__action--negative");
 
 selectPlanButtons.forEach(button => {
     button.addEventListener("click", () => {
@@ -8,3 +9,12 @@ selectPlanButtons.forEach(button => {
         backdrop[0].style.display = "block";
     })
 })
+
+const closeModal = () => {
+    modal.style.display = "none";
+    backdrop[0].style.display = "none";
+}
+
+backdrop[0].addEventListener("click", closeModal);
+
+modalNoButton.addEventListener("click", closeModal);
