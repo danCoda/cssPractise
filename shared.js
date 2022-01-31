750bb724eac250deffa8a41c1b1b49f54e584548
Daniel Son <dson.email@gmail.com>
2022-02-01 08:04:54 +1100

    Add hamburger to all pages

--- shared.js
+++ shared.js
@@ -7,24 +7,31 @@ const mobileNav = document.querySelector(".mobile-nav");
 
 selectPlanButtons.forEach(button => {
     button.addEventListener("click", () => {
-        modal.style.display = "block";
-        backdrop[0].style.display = "block";
+        /* modal.style.display = "block";
+        backdrop[0].style.display = "block"; */
+        modal.classList.add("open");
+        backdrop[0].classList.add("open");
     })
 })
 
 const closeModal = () => {
-    modal.style.display = "none";
-    backdrop[0].style.display = "none";
+    /* modal.style.display = "none";
+    backdrop[0].style.display = "none"; */
+    if (modal) modal.classList.remove("open");
+    backdrop[0].classList.remove("open");
 }
 
 backdrop[0].addEventListener("click", () => {
-    mobileNav.style.display = "none";
+    //mobileNav.style.display = "none";
+    mobileNav.classList.remove("open");
     closeModal();
 });
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) modalNoButton.addEventListener("click", closeModal);
 
 toggleButton.addEventListener("click", () => {
-    mobileNav.style.display = "block";
-    backdrop[0].style.display = "block";
+    /* mobileNav.style.display = "block";
+    backdrop[0].style.display = "block"; */
+    mobileNav.classList.add("open");
+    backdrop[0].classList.add("open");
 })
