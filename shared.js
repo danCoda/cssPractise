f400fff320bfb7c7c16098097ff3946562c87cbe
Daniel Son <dson.email@gmail.com>
2022-02-01 07:45:50 +1100

    Add open / close functionality for hamburger

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelectorAll(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan button");
 const modalNoButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 selectPlanButtons.forEach(button => {
     button.addEventListener("click", () => {
@@ -15,6 +17,14 @@ const closeModal = () => {
     backdrop[0].style.display = "none";
 }
 
-backdrop[0].addEventListener("click", closeModal);
+backdrop[0].addEventListener("click", () => {
+    mobileNav.style.display = "none";
+    closeModal();
+});
 
 modalNoButton.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", () => {
+    mobileNav.style.display = "block";
+    backdrop[0].style.display = "block";
+})
